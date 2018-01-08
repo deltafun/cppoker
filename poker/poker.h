38c43447ca4b5a9dba1e22d44b26a9bb22992b52
Daniel <daniel.parra3@outlook.com>
2018-01-08 00:01:05 -0500

    Add project files.

---
+++ poker/poker.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "stdafx.h"
+
+enum class Suit { Diamonds, Hearts, Clubs, Spades };
+
+enum class Rank {
+    Two, Three, Four, Five, Six, Seven, Eight, Nine, Jack, Queen, King, Ace
+};
+
+struct Card {
+    const Rank rank;
+    const Suit suit;
+};
+
+struct PokerHand {
+    const std::array<Card, 5> cards;
+    const Rank highCard;
+};
+
+std::vector<std::string> SplitInputHands(std::string);
+std::vector<std::vector<std::string>> SplitHands(std::vector<std::string>);
---
+++ test/test.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "poker.h"
+
+TEST_CASE("Input string is split into two poker hand strings") {
+    
+}
