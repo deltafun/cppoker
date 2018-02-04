0bce4fcfad67abb80ae93c1900314984d81d9de4
Daniel <daniel.parra3@outlook.com>
2018-02-03 22:06:39 -0500

    Add hand creation logic and tests.

--- poker/poker.h
+++ poker/poker.h
@@ -2,24 +2,27 @@
 
 #include "stdafx.h"
 
+namespace Data {
+
+    const std::map<char, int> Ranks = {
+        { '2', 2 }, { '3', 3 }, { '4', 4 }, { '5', 5 }, { '6', 6 }, { '7', 7 },
+        { '8', 8 }, { '9', 9 }, 
+        { 'J', 10 }, { 'Q', 11 }, { 'K', 12 }, { 'A', 13 }
+    };
+
+    struct Card {
+        int rank;
+        char suit;
+    };
+
+    inline bool operator==(const Card& lhs, const Card& rhs) {
+        return lhs.rank == rhs.rank && lhs.suit == rhs.suit;
+    }
+}
+
 namespace Parser {
     std::array<std::array<std::string, 5>, 2> 
                                     SplitInputHands(const std::string& hands);
+    std::vector<Data::Card> CreateHand(
+        const std::array<std::string, 5> hand);
 }
-
-
-enum class Suit { Diamonds, Hearts, Clubs, Spades };
-
-enum class Rank {
-    Two, Three, Four, Five, Six, Seven, Eight, Nine, Jack, Queen, King, Ace
-};
-
-struct Card {
-    const Rank rank;
-    const Suit suit;
-};
-
-struct PokerHand {
-    const std::array<Card, 5> cards;
-    const Rank highCard;
-};
--- test/test.cpp
+++ test/test.cpp
@@ -15,3 +15,15 @@ TEST_CASE("Input string is split into two poker hand strings") {
     REQUIRE(expect[0] == actual[0]);
     REQUIRE(expect[1] == actual[1]);
 }
+
+TEST_CASE("Poker hand string is converted in poker hand vector of Cards") {
+    std::vector<Data::Card> expect = {
+        Data::Card {2, 'H'}, Data::Card {3, 'D'}, Data::Card {5, 'S'},
+        Data::Card {9, 'C'}, Data::Card {12, 'D'}
+    };
+
+    std::array<std::string, 5> input = { "2H", "3D", "5S", "9C", "KD" };
+    std::vector<Data::Card> actual = Parser::CreateHand(input);
+
+    REQUIRE(expect == actual);
+}
